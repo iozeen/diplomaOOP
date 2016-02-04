4e3a7e34ef999cbd8c5e73c6e40099d255b3688f
a-tyshchenko <a-tyshchenko@i.ua>
2016-02-04 21:29:11 +0200

    change HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
         System.out.println("Hello Sanya");
         System.out.println();
         System.out.println();System.out.println();
-        System.out.println();
+        System.out.println("Hi");
     }
 }
