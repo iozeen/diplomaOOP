3d27533ac09f117e7351a027b6569b8e6910f044
iozeen <iozintv@gmail.com>
2016-02-09 21:01:23 +0200

    Student simple change

--- src/course/Student.java
+++ src/course/Student.java
@@ -2,27 +2,18 @@ package course;
 
 
 public class Student extends Person {
-    private double sum;
-    private int idStudent;
-    private static int idNext = 1;
+    private int sessionSum;
+    private Level level;
+//    private int idStudent;
+//    private static int idNext = 1;
 
-    public Student(){
-        idStudent = idNext++;
-    }
+//    public Student(){
+//        idStudent = idNext++;
+//    }
     public Student( String name, String surname){
         super();
         setName(name);
         setSurname(surname);
     }
-    public double getSum() {
-        return sum;
-    }
 
-    public int getIdStudent() {
-        return idStudent;
-    }
-
-    public static int getIdNext() {
-        return idNext;
-    }
 }
