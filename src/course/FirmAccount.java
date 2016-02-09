cce8a4c4f9c033c2cec78440494bc47d4464d1a5
a-tyshchenko <a-tyshchenko@i.ua>
2016-02-09 20:32:17 +0200

    Commit

---
+++ src/course/FirmAccount.java
@@ -0,0 +1,38 @@
+package course;
+
+import java.util.List;
+
+
+public class FirmAccount {
+    private static int balance;
+    private static int sessionCost;
+    private static FirmAccount firmAccount = new FirmAccount();
+    private static List<Student> studentList;
+    private static List<Group> groupList;
+
+
+
+    private FirmAccount() {
+
+    }
+
+    public static FirmAccount getFirmAccount() {
+        return firmAccount;
+    }
+
+    public static void updateBalance(int sum) {
+        balance = balance + sum;
+    }
+
+    public static void setSessionCost(int sum) {
+        sessionCost = sum;
+    }
+    public static List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public static List<Group> getGroupList() {
+        return groupList;
+    }
+
+}
--- src/course/Group.java
+++ src/course/Group.java
@@ -6,4 +6,36 @@ import java.util.List;
 
 public class Group {
     private List<Student> studentList = new ArrayList<>();
+    private Instructor instructor;
+    private Level levelGroup;
+
+    public Group() {
+
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public Level getLevelGroup() {
+        return levelGroup;
+    }
+
+    public void setLevelGroup(Level levelGroup) {
+        this.levelGroup = levelGroup;
+    }
+
+    public boolean addStudent(Student student) {
+        if (true) { // realizovat' provarky na nalichie yge etogo studenta
+            studentList.add(student);
+            return true;
+        }
+        return false;
+    }
+
+
 }
