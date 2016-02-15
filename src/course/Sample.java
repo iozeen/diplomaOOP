295a70b2ca8a73602f1ad656340c3aab15f09b4e
iozeen <iozintv@gmail.com>
2016-02-15 17:45:55 +0200

    Changes to branch ilya

--- src/course/Sample.java
+++ src/course/Sample.java
@@ -7,5 +7,8 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("New changes.");
         System.out.println("New changes.");
+        System.out.println("New changes.");
+        System.out.println("New changes.");
+        System.out.println("New changes.");
     }
 }
